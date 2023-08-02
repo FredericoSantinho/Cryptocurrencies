fee65282d7b5961f6d5ed9983766badd7f44c05a
Neurophobic Animal <FredericoSantinho@gmail.com>
2023-08-02 20:50:22 +0100

    Implemented FetchCoinDetailsUseCaseImplTest.kt.

---
+++ domain/src/test/java/neuro/cryptocurrencies/domain/usecase/coin/details/FetchCoinDetailsUseCaseImplTest.kt
@@ -0,0 +1,73 @@
+package neuro.cryptocurrencies.domain.usecase.coin.details
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.runBlocking
+import neuro.cryptocurrencies.domain.entity.CoinDetails
+import neuro.cryptocurrencies.domain.entity.CoinTicker
+import neuro.cryptocurrencies.domain.repository.coin.GetCoinTickerRepository
+import neuro.cryptocurrencies.domain.repository.coin.details.GetCoinDetailsRepository
+import neuro.cryptocurrencies.domain.repository.coin.details.SaveCoinDetailsWithPriceRepository
+import org.junit.Test
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.times
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.verifyNoInteractions
+import org.mockito.kotlin.whenever
+
+class FetchCoinDetailsUseCaseImplTest {
+	@Test
+	fun test() = runBlocking {
+		val getCoinTickerRepository = mock<GetCoinTickerRepository>()
+		val getCoinDetailsRepository = mock<GetCoinDetailsRepository>()
+		val saveCoinDetailsWithPriceRepository = mock<SaveCoinDetailsWithPriceRepository>()
+
+		val fetchCoinDetailsUseCase = FetchCoinDetailsUseCaseImpl(
+			getCoinTickerRepository,
+			getCoinDetailsRepository,
+			saveCoinDetailsWithPriceRepository
+		)
+
+		val coinId = "btc-bitcoin"
+		val price = 25423.0
+		whenever(getCoinTickerRepository.getCoinTicker(coinId)).thenReturn(
+			CoinTicker(
+				"1",
+				"Bitcoin",
+				1,
+				"BTC",
+				price
+			)
+		)
+		val coinDetails = CoinDetails(
+			"1",
+			"description",
+			"SHA256",
+			"",
+			"Bitcoin",
+			true,
+			"POW",
+			1,
+			"BTC",
+			emptyList(),
+			emptyList(),
+			"coin"
+		)
+		whenever(getCoinDetailsRepository.getCoinDetails(coinId)).thenReturn(
+			coinDetails
+		)
+
+		verifyNoInteractions(getCoinTickerRepository)
+		verifyNoInteractions(getCoinDetailsRepository)
+		verifyNoInteractions(saveCoinDetailsWithPriceRepository)
+
+		fetchCoinDetailsUseCase.execute(coinId, CoroutineScope(Job()))
+
+		verify(getCoinTickerRepository, times(1)).getCoinTicker(coinId)
+		verify(getCoinDetailsRepository, times(1)).getCoinDetails(coinId)
+		verify(saveCoinDetailsWithPriceRepository, times(1)).saveCoinDetailsWithPrice(
+			coinDetails,
+			price
+		)
+	}
+}
