f821fc6d19870cfdac8931a22f5a17142bf91d7d
Neurophobic Animal <FredericoSantinho@gmail.com>
2023-08-03 21:08:04 +0100

    Implemented CoinListViewModelImpl.kt testNetworkErrorWithCachedCoinsTickers.

--- presentation/src/test/java/neuro/cryptocurrencies/presentation/viewmodel/coins/CoinListViewModelImplTest.kt
+++ presentation/src/test/java/neuro/cryptocurrencies/presentation/viewmodel/coins/CoinListViewModelImplTest.kt
@@ -100,4 +100,41 @@ class CoinListViewModelImplTest {
 			)
 		}
 	}
+
+	@Test
+	fun testNetworkErrorWithCachedCoinsTickers() = runTest {
+		val observeCoinsTickersUseCase = mock<ObserveCoinsTickersUseCase>()
+		val fetchCoinsTickersUseCase = mock<FetchCoinsTickersUseCase>()
+		val hasCachedCoinsTickersUseCase = mock<HasCachedCoinsTickersUseCase>()
+		val testIoDispatcher = StandardTestDispatcher()
+
+		val errorMessage = "someError"
+		whenever(observeCoinsTickersUseCase.execute()).thenReturn(flow { emit(coinTickerMockList()) })
+		whenever(fetchCoinsTickersUseCase.execute()).thenThrow(RuntimeException(errorMessage))
+		whenever(hasCachedCoinsTickersUseCase.execute()).thenReturn(true)
+
+		val coinListViewModel = CoinListViewModelImpl(
+			observeCoinsTickersUseCase,
+			fetchCoinsTickersUseCase,
+			hasCachedCoinsTickersUseCase,
+			testIoDispatcher
+		)
+
+		assertEquals(CoinListState(isLoading = true), coinListViewModel.uiState.value)
+
+		verifyNoInteractions(fetchCoinsTickersUseCase)
+
+		testIoDispatcher.scheduler.runCurrent()
+
+		verify(fetchCoinsTickersUseCase, times(1)).execute()
+
+		assertEquals(
+			CoinListState(
+				coins = coinTickerModelMockList(),
+				isError = false,
+				isLoading = false,
+				isRefreshing = false
+			), coinListViewModel.uiState.value
+		)
+	}
 }
