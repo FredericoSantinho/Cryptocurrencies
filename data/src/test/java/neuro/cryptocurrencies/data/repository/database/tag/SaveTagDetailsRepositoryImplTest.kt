b0dba36e3a573ea3519cd2cb61b0fb889907f6a8
Neurophobic Animal <FredericoSantinho@gmail.com>
2023-08-03 16:51:57 +0100

    Implemented SaveTagDetailsRepositoryImplTest.kt.

---
+++ data/src/test/java/neuro/cryptocurrencies/data/repository/database/tag/SaveTagDetailsRepositoryImplTest.kt
@@ -0,0 +1,27 @@
+package neuro.cryptocurrencies.data.repository.database.tag
+
+import kotlinx.coroutines.test.runTest
+import neuro.cryptocurrencies.data.dao.TagDetailsDao
+import neuro.cryptocurrencies.data.mocks.database.roomTagDetailsMock
+import neuro.cryptocurrencies.domain.mocks.tagDetailsMock
+import org.junit.Test
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.times
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.verifyNoInteractions
+
+class SaveTagDetailsRepositoryImplTest {
+
+	@Test
+	fun test() = runTest {
+		val tagDetailsDao = mock<TagDetailsDao>()
+
+		val saveTagDetailsRepository = SaveTagDetailsRepositoryImpl(tagDetailsDao)
+
+		verifyNoInteractions(tagDetailsDao)
+
+		saveTagDetailsRepository.saveTagDetails(tagDetailsMock())
+
+		verify(tagDetailsDao, times(1)).upsertTagDetails(roomTagDetailsMock())
+	}
+}
