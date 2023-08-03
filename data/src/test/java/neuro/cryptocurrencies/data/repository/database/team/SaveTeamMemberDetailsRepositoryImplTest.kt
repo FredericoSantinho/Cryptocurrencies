b24f57f7732cde47ca5d19ba33a1f6f1393ba5e5
Neurophobic Animal <FredericoSantinho@gmail.com>
2023-08-03 16:58:19 +0100

    Implemented SaveTeamMemberDetailsRepositoryImplTest.kt.

---
+++ data/src/test/java/neuro/cryptocurrencies/data/repository/database/team/SaveTeamMemberDetailsRepositoryImplTest.kt
@@ -0,0 +1,27 @@
+package neuro.cryptocurrencies.data.repository.database.team
+
+import kotlinx.coroutines.test.runTest
+import neuro.cryptocurrencies.data.dao.TeamDetailsDao
+import neuro.cryptocurrencies.data.mocks.database.roomTeamMemberDetailsMock
+import neuro.cryptocurrencies.domain.mocks.teamMemberDetailsMock
+import org.junit.Test
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.times
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.verifyNoInteractions
+
+class SaveTeamMemberDetailsRepositoryImplTest {
+
+	@Test
+	fun test() = runTest {
+		val teamDetailsDao = mock<TeamDetailsDao>()
+
+		val saveTeamMemberDetailsRepository = SaveTeamMemberDetailsRepositoryImpl(teamDetailsDao)
+
+		verifyNoInteractions(teamDetailsDao)
+
+		saveTeamMemberDetailsRepository.saveTeamMemberDetails(teamMemberDetailsMock())
+
+		verify(teamDetailsDao, times(1)).upsertTeamMemberDetails(roomTeamMemberDetailsMock())
+	}
+}
