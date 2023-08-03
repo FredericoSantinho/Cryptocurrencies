3c4c5ee138a807c44231042679fc05c90cca5bc5
Neurophobic Animal <FredericoSantinho@gmail.com>
2023-08-03 15:09:02 +0100

    Implemented HasCachedCoinDetailsRepositoryImplTest.kt.

---
+++ data/src/test/java/neuro/cryptocurrencies/data/repository/database/coin/details/HasCachedCoinDetailsRepositoryImplTest.kt
@@ -0,0 +1,50 @@
+package neuro.cryptocurrencies.data.repository.database.coin.details
+
+import kotlinx.coroutines.runBlocking
+import neuro.cryptocurrencies.data.dao.CoinDetailsDao
+import org.junit.Test
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.times
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.verifyNoInteractions
+import org.mockito.kotlin.whenever
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+class HasCachedCoinDetailsRepositoryImplTest {
+	@Test
+	fun hasCoinDetails() = runBlocking {
+		val coinDetailsDao = mock<CoinDetailsDao>()
+
+		val hasCachedCoinDetailsRepository = HasCachedCoinDetailsRepositoryImpl(coinDetailsDao)
+
+		val coinId = "btc-bitcoin"
+		whenever(coinDetailsDao.hasCoinDetails(coinId)).thenReturn(true)
+
+		verifyNoInteractions(coinDetailsDao)
+
+		val hasCachedCoinDetails = hasCachedCoinDetailsRepository.hasCachedCoinDetails(coinId)
+
+		verify(coinDetailsDao, times(1)).hasCoinDetails(coinId)
+
+		assertTrue(hasCachedCoinDetails)
+	}
+
+	@Test
+	fun `doesn'tHaveCoinDetails`() = runBlocking {
+		val coinDetailsDao = mock<CoinDetailsDao>()
+
+		val hasCachedCoinDetailsRepository = HasCachedCoinDetailsRepositoryImpl(coinDetailsDao)
+
+		val coinId = "btc-bitcoin"
+		whenever(coinDetailsDao.hasCoinDetails(coinId)).thenReturn(false)
+
+		verifyNoInteractions(coinDetailsDao)
+
+		val hasCachedCoinDetails = hasCachedCoinDetailsRepository.hasCachedCoinDetails(coinId)
+
+		verify(coinDetailsDao, times(1)).hasCoinDetails(coinId)
+
+		assertFalse(hasCachedCoinDetails)
+	}
+}
