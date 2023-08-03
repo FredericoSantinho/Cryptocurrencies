099d3342f124d2b32293f7f3d09dbaf6eb862efd
Neurophobic Animal <FredericoSantinho@gmail.com>
2023-08-03 16:57:03 +0100

    Implemented ObserveTeamMemberDetailsRepositoryImplTest.kt.

---
+++ data/src/test/java/neuro/cryptocurrencies/data/mocks/database/RoomTeamMemberDetailsMock.kt
@@ -0,0 +1,6 @@
+package neuro.cryptocurrencies.data.mocks.database
+
+import neuro.cryptocurrencies.data.model.database.team.RoomTeamMemberDetails
+
+fun roomTeamMemberDetailsMock() =
+	RoomTeamMemberDetails("satoshi-nakamoto", "Satoshi Nakamoto", "description")
---
+++ data/src/test/java/neuro/cryptocurrencies/data/repository/database/team/ObserveTeamMemberDetailsRepositoryImplTest.kt
@@ -0,0 +1,41 @@
+package neuro.cryptocurrencies.data.repository.database.team
+
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.test.runTest
+import neuro.cryptocurrencies.data.dao.TeamDetailsDao
+import neuro.cryptocurrencies.data.mocks.database.roomTeamMemberDetailsMock
+import neuro.cryptocurrencies.domain.mocks.teamMemberDetailsMock
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.times
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.verifyNoInteractions
+import org.mockito.kotlin.whenever
+
+class ObserveTeamMemberDetailsRepositoryImplTest {
+
+	@Test
+	fun test() = runTest {
+		val teamDetailsDao = mock<TeamDetailsDao>()
+
+		val observeTeamMemberDetailsRepository = ObserveTeamMemberDetailsRepositoryImpl(teamDetailsDao)
+
+		val teamMemberId = "satoshi-nakamoto"
+		whenever(teamDetailsDao.observeTeamMemberDetails(teamMemberId)).thenReturn(flow {
+			emit(
+				roomTeamMemberDetailsMock()
+			)
+		})
+
+		verifyNoInteractions(teamDetailsDao)
+
+		val teamMemberDetails =
+			observeTeamMemberDetailsRepository.observeTeamMemberDetails(teamMemberId).first()
+
+		verify(teamDetailsDao, times(1)).observeTeamMemberDetails(teamMemberId)
+
+		assertEquals(teamMemberDetailsMock(), teamMemberDetails)
+	}
+}
