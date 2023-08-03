44fad04efb7c1bd0df25f2128b99b82b3b4a3cb1
Neurophobic Animal <FredericoSantinho@gmail.com>
2023-08-03 16:50:20 +0100

    Implemented ObserveTagDetailsRepositoryImplTest.kt.

---
+++ data/src/test/java/neuro/cryptocurrencies/data/mocks/database/RoomTagDetailsMock.kt
@@ -0,0 +1,5 @@
+package neuro.cryptocurrencies.data.mocks.database
+
+import neuro.cryptocurrencies.data.model.database.tag.RoomTagDetails
+
+fun roomTagDetailsMock() = RoomTagDetails("segwit", "Segwit", "description")
---
+++ data/src/test/java/neuro/cryptocurrencies/data/repository/database/tag/ObserveTagDetailsRepositoryImplTest.kt
@@ -0,0 +1,40 @@
+package neuro.cryptocurrencies.data.repository.database.tag
+
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.test.runTest
+import neuro.cryptocurrencies.data.dao.TagDetailsDao
+import neuro.cryptocurrencies.data.mocks.database.roomTagDetailsMock
+import neuro.cryptocurrencies.domain.mocks.tagDetailsMock
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.times
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.verifyNoInteractions
+import org.mockito.kotlin.whenever
+
+class ObserveTagDetailsRepositoryImplTest {
+
+	@Test
+	fun test() = runTest {
+		val tagDetailsDao = mock<TagDetailsDao>()
+
+		val observeTagDetailsRepository = ObserveTagDetailsRepositoryImpl(tagDetailsDao)
+
+		val tagId = "segwit"
+		whenever(tagDetailsDao.observeTagDetails(tagId)).thenReturn(flow {
+			emit(
+				roomTagDetailsMock()
+			)
+		})
+
+		verifyNoInteractions(tagDetailsDao)
+
+		val coinTickerList = observeTagDetailsRepository.observeTagDetails(tagId).first()
+
+		verify(tagDetailsDao, times(1)).observeTagDetails(tagId)
+
+		assertEquals(tagDetailsMock(), coinTickerList)
+	}
+}
