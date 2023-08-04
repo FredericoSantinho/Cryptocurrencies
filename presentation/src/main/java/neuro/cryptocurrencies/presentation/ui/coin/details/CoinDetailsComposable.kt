6251a3f5e874660d4d695bb85484acb2f8259fbb
Neurophobic Animal <FredericoSantinho@gmail.com>
2023-08-04 19:26:55 +0100

    Add coin logo to CoinDetailsComposable.kt.

--- presentation/src/main/java/neuro/cryptocurrencies/presentation/ui/coin/details/CoinDetailsComposable.kt
+++ presentation/src/main/java/neuro/cryptocurrencies/presentation/ui/coin/details/CoinDetailsComposable.kt
@@ -1,14 +1,17 @@
 package neuro.cryptocurrencies.presentation.ui.coin.details
 
 import android.widget.Toast
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.CircularProgressIndicator
@@ -32,12 +35,14 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import coil.compose.rememberAsyncImagePainter
 import com.google.accompanist.flowlayout.FlowRow
 import neuro.cryptocurrencies.presentation.R
 import neuro.cryptocurrencies.presentation.mapper.toPresentation
@@ -106,6 +111,24 @@ fun CoinDetailsComposable(
 								modifier = Modifier.padding(16.dp),
 								verticalArrangement = Arrangement.spacedBy(16.dp),
 							) {
+								if (coinDetailsModel.logo.isNotBlank()) {
+									item {
+										Row(
+											modifier = Modifier.fillMaxWidth(),
+											horizontalArrangement = Arrangement.Center
+										) {
+											Image(
+												modifier = Modifier
+													.size(64.dp),
+												painter = rememberAsyncImagePainter(
+													coinDetailsModel.logo
+												),
+												contentDescription = null,
+												contentScale = ContentScale.Crop
+											)
+										}
+									}
+								}
 								item {
 									CoinListItemComposable(
 										coinDetailsModel.rank,
